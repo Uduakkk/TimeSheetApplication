053cd8a67d07e2e1aa13d61285a613a780b352b5
Uduakkk <uduakumanah99@gmail.com>
2023-12-01 11:17:21 +0100

    User interface

--- TimesheetApplication/Models/CreateUser.cs
+++ TimesheetApplication/Models/CreateUser.cs
@@ -13,6 +13,7 @@ namespace TimesheetApplication.Models
         [EmailAddress(ErrorMessage = "Invaliid email address")]
         public string Email { get; set; } = string.Empty;
         [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,}$", ErrorMessage = "at least 8 characters, at least one uppercase, at least one lowercase, at least one digit, at least one special character")]
         public string Password { get; set; } = string.Empty;
         [Required(ErrorMessage = "Username is required")]
